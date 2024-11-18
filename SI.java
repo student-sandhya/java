9520b9246d53af41e3c44080059db6aad0b8f702
student-sandhya <sandhyaranipradhan626@gmail.com>
2024-11-18 20:32:57 +0530

    Add files via upload

---
+++ SI.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class SI{
+    public static void main(String A[]){
+        int P=10000;
+        double R=2.5;
+        int T=3;
+        System.out.println("SI="+(P*R*T)/100);
+    }
+}
+
+
+
